0e9b9da11a2ed1f69bf0f8d10682ad5257015c93
Mauricio Fullana <mauriciof@bankingly.com>
2020-10-15 21:44:46 -0300

    Minor changes.

--- app/src/main/java/com/example/qrandbarcodescanner/MainActivity.java
+++ app/src/main/java/com/example/qrandbarcodescanner/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
@@ -17,12 +18,14 @@ import com.google.zxing.integration.android.IntentResult;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button btnScan;
+    TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtResult = findViewById(R.id.txt_result);
         btnScan = findViewById(R.id.btn_scan);
         btnScan.setOnClickListener(this);
     }
@@ -46,8 +49,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() != null) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage(result.getContents());
+
+                String scanResult = result.getContents();
+                txtResult.setText(scanResult);
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(scanResult);
+                builder.setCancelable(false);
                 builder.setTitle("Scanning Result");
                 builder.setPositiveButton("Scan Again", new DialogInterface.OnClickListener() {
                     @Override
@@ -57,7 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }).setNegativeButton("Finish", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        finish();
+
                     }
                 });
                 AlertDialog dialog = builder.create();
